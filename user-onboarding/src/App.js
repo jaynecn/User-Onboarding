26904be6bcaa46a00842bd8ac6eab5889e291f65
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-19 14:35:03 +0100

    created form, now creating axios requests

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import Form from './Form';
+import React, { useState, useEffect} from 'react';
+// import Formik  from 'formik';
+// import * as Yup from 'yup';
+import axios from 'axios';
+import FormikLoginForm from './Form';
 import './App.css';
+
+const initialForm = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 
 function App() {
+  const [ usersList, setUsersList] = useState([]);
+  const [ serverError, setServerError] = useState('');
+
+  const fetchUsers = () => {
+    axios.get('https://reqres.in/api/users_')
+      .then(response => {
+        setUsersList(response.data);
+      })
+      .catch(error => {
+        setServerError(error.message);
+      })
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <Form />
+      <FormikLoginForm />
       
     </div>
   );
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function Form() {
+function LoginForm(props) {
   return (
-    <div>
-      This is the form.
-    </div>
-  )
+    <Form>
+      <div>
+        <Field name='name' type='text' placeholder='Name' />
+      </div>
+      <div>
+        <Field name='email' type='email' placeholder='Email' />
+      </div>
+      <div>
+        <Field name='password' type='password' placeholder='Password' />
+      </div>
+      <label>
+        <Field type="checkbox" name="tos" checked={props.tos} />
+        Accept TOS
+      </label>
+      <button type='submit'>Submit</button>
+    </Form>
+  );
 }
 
-export default Form;
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ name, email, password, tos}) {
+    return {
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      tos: tos || false,
+    }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email().required('Email is required'),
+    password: Yup.string().min(6, 'Password must be a least 6 characters').required('Please create a password')
+  }),
+
+})(LoginForm);
+
+export default FormikLoginForm;
